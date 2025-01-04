fd090d19e25d327453a0c2b9301c64fd143bbdc3
sanket-oriens <sanketc1297@gmail.com>
2025-01-04 08:30:28 +0530

    first commit

---
+++ spring-sec-1/src/main/java/com/project2/sec/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.project2.sec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+	
+	@GetMapping("/")
+	public String getHi() {
+		
+		return "Hi";
+	}
+
+}
